1a93ea32ffd97a4e5c73ed7705ec9108a52c38dd
OuambVision <ouambvision@gmail.com>
2024-07-16 02:17:35 -0400

    route OK

--- app/api/connexion/route.js
+++ app/api/connexion/route.js
@@ -1,4 +1,12 @@
 //Route pour retourner bonjour
-app.get('/bonjour', (req, res) => {
-  res.send('Bonjour');
-});
+import { NextResponse } from "next/server";
+
+export async function GET(request){
+  try{
+    return NextResponse.json({status: 200})
+  }
+  catch(error){
+      console.log(error)
+      return NextResponse.json({erruer : "Erreur lors de la tentative"}, {status: 500})
+  }
+}
